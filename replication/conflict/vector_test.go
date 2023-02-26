6cdfabd9f14519f189539d6e68c5957dfbcc15a5
Hebella <2701970300@qq.com>
2023-02-26 18:00:35 -0500

    add test

--- replication/conflict/vector_test.go
+++ replication/conflict/vector_test.go
@@ -19,3 +19,37 @@ func TestVectorConcurrentEventsDoNotHappenBefore(t *testing.T) {
 		t.Errorf("v1 does not happen before v2 due to v2[0] > v1[0]")
 	}
 }
+
+func TestResolveConcurrentEventsConflictsVector(t *testing.T) {
+	r := NewVersionVectorConflictResolver()
+
+	v1 := NewVersionVectorClock()
+	v2 := NewVersionVectorClock()
+
+	v1.vector[0] = 0
+	v1.vector[3] = 2
+	v2.vector[0] = 1
+
+	kv1 := KVFromParts[VersionVectorClock]("key", "value1", v1)
+	kv2 := KVFromParts[VersionVectorClock]("key", "value2", v2)
+
+	kv, err := r.ResolveConcurrentEvents(kv1, kv2)
+	if err != nil {
+		t.Errorf("error should be nil")
+	}
+	if kv.Value != "value2" {
+		t.Errorf("Resolver should choose the key-value with the highest lexicographic value")
+	}
+	if kv.Clock.vector[0] != 1 || kv.Clock.vector[3] != 2 {
+		t.Errorf("Resolver should return a higher clock")
+	}
+}
+
+func TestResolveConcurrentEventsNoConflictsVector(t *testing.T) {
+	r := NewVersionVectorConflictResolver()
+
+	_, err := r.ResolveConcurrentEvents()
+	if err == nil {
+		t.Errorf("error should not be nil")
+	}
+}
