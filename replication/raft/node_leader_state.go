783c87fcb7df11bb7fe94c286467e97a774bbfc8
jingxian2022 <jingxian.zh@outlook.com>
2023-05-01 01:00:32 -0400

    Fix ?

--- replication/raft/node_leader_state.go
+++ replication/raft/node_leader_state.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	pb "modist/proto"
 	"sync"
-	"time"
 )
 
 func (rn *RaftNode) sendHeartbeat(nextState chan stateFunction) {
@@ -46,45 +45,43 @@ func (rn *RaftNode) sendHeartbeat(nextState chan stateFunction) {
 	wg0.Wait()
 
 	// send heartbeats to all servers periodically
-	heartbeatTicker := time.NewTicker(rn.heartbeatTimeout)
-	defer heartbeatTicker.Stop()
+	// heartbeatTicker := time.NewTicker(rn.heartbeatTimeout)
+	// defer heartbeatTicker.Stop()
 
-	var wg sync.WaitGroup
-	for {
-		if rn.state != LeaderState {
-			return
-		}
-		select {
-		case <-heartbeatTicker.C:
-			for followerNodeID := range followerNodes {
-				wg.Add(1)
-				go func(id uint64) {
-					// if id == rn.node.ID {
-					// 	wg.Done()
-					// 	return
-					// }
-					defer wg.Done()
-					rn.log.Printf("leader %d is sending heartbeat to %d", rn.node.ID, id)
-					conn := rn.node.PeerConns[uint64(id)]
-					follower := pb.NewRaftRPCClient(conn)
-					reply, err := follower.AppendEntries(ctx, &pb.AppendEntriesRequest{ //FIXME:
-						From:         rn.node.ID,
-						To:           uint64(id),
-						Term:         rn.GetCurrentTerm(),
-						LeaderCommit: rn.commitIndex,
-					})
-					if err != nil {
-						rn.log.Printf("error sending heartbeat to %d: %v", id, err)
-					}
-					if !reply.GetSuccess() {
-						rn.log.Printf("follower %d is out of date", id)
-						nextState <- rn.doFollower
-					}
-				}(followerNodeID)
-			}
-			wg.Wait()
-		}
-	}
+	// var wg sync.WaitGroup
+	// for {
+	// 	if rn.state != LeaderState {
+	// 		return
+	// 	}
+	// 	<-heartbeatTicker.C
+	// 	for followerNodeID := range followerNodes {
+	// 		wg.Add(1)
+	// 		go func(id uint64) {
+	// 			// if id == rn.node.ID {
+	// 			// 	wg.Done()
+	// 			// 	return
+	// 			// }
+	// 			defer wg.Done()
+	// 			rn.log.Printf("leader %d is sending heartbeat to %d", rn.node.ID, id)
+	// 			conn := rn.node.PeerConns[uint64(id)]
+	// 			follower := pb.NewRaftRPCClient(conn)
+	// 			reply, err := follower.AppendEntries(ctx, &pb.AppendEntriesRequest{ //FIXME:
+	// 				From:         rn.node.ID,
+	// 				To:           uint64(id),
+	// 				Term:         rn.GetCurrentTerm(),
+	// 				LeaderCommit: rn.commitIndex,
+	// 			})
+	// 			if err != nil {
+	// 				rn.log.Printf("error sending heartbeat to %d: %v", id, err)
+	// 			}
+	// 			if !reply.GetSuccess() {
+	// 				rn.log.Printf("follower %d is out of date", id)
+	// 				nextState <- rn.doFollower
+	// 			}
+	// 		}(followerNodeID)
+	// 	}
+	// 	wg.Wait()
+	// }
 }
 
 func (rn *RaftNode) leaderListen(nextStateC chan stateFunction) {
